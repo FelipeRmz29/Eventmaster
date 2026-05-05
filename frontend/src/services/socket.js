76d7431689dab2db858663de3b4727c69c785692
Javalogo2285 <Javierlozanoglez@gmail.com>
2026-05-05 16:59:31 -0600

    feat(Frontend):Actualizancin de asientos y socket.

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,8 +1,13 @@
 let socket = null;
 
+const getSocketUrl = () => {
+  const host = window.location.hostname;
+  return `ws://${host}:3000`;
+};
+
 export const connectSocket = () => {
-  if (!socket) {
-    socket = new WebSocket("ws://localhost:3000");
+  if (!socket || socket.readyState === WebSocket.CLOSED) {
+    socket = new WebSocket(getSocketUrl());
   }
   return socket;
 };
